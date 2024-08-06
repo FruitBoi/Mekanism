edd7248aae09b2baf13c5f3173c7e187d274c156
Fruit Boi <lemonfruit@meth.expert>
2024-08-06 02:42:06 -0500

    Move water output from turbine vents to turbine valves to reduce tick time

--- src/generators/java/mekanism/generators/common/tile/turbine/TileEntityTurbineVent.java
+++ src/generators/java/mekanism/generators/common/tile/turbine/TileEntityTurbineVent.java
@@ -14,43 +14,9 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
-public class TileEntityTurbineVent extends TileEntityTurbineCasing implements IMultiblockEjector {
-
-    private Set<Direction> outputDirections = Collections.emptySet();
+public class TileEntityTurbineVent extends TileEntityTurbineCasing {
 
     public TileEntityTurbineVent(BlockPos pos, BlockState state) {
         super(GeneratorsBlocks.TURBINE_VENT, pos, state);
-    }
-
-    @NotNull
-    @Override
-    protected IFluidTankHolder getInitialFluidTanks(IContentsListener listener) {
-        return side -> {
-            TurbineMultiblockData multiblock = getMultiblock();
-            return multiblock.isFormed() ? multiblock.ventTanks : Collections.emptyList();
-        };
-    }
-
-    @Override
-    protected boolean onUpdateServer(TurbineMultiblockData multiblock) {
-        boolean needsPacket = super.onUpdateServer(multiblock);
-        if (multiblock.isFormed()) {
-            FluidUtils.emit(outputDirections, multiblock.ventTank, this);
-        }
-        return needsPacket;
-    }
-
-    @Override
-    public boolean persists(SubstanceType type) {
-        //Do not handle fluid when it comes to syncing it/saving this tile to disk
-        if (type == SubstanceType.FLUID) {
-            return false;
-        }
-        return super.persists(type);
-    }
-
-    @Override
-    public void setEjectSides(Set<Direction> sides) {
-        outputDirections = sides;
     }
 }
